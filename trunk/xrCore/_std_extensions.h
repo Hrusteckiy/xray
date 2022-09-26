7e4df5c3949b8774324d9bdea6979de929e49500
Hrusteckiy <kostia4554t@yandex.ru>
2022-09-27 02:28:43 +0300

    xr_strcpy

--- trunk/xrCore/_std_extensions.h
+++ trunk/xrCore/_std_extensions.h
@@ -101,14 +101,14 @@ IC float	_sin	(float x)		{ return sinf(x); }
 IC float	_cos	(float x)		{ return cosf(x); }
 IC BOOL		_valid	(const float x)
 {
-	// check for: Signaling NaN, Quiet NaN, Negative infinity ( –INF), Positive infinity (+INF), Negative denormalized, Positive denormalized
+	// check for: Signaling NaN, Quiet NaN, Negative infinity ( â€“INF), Positive infinity (+INF), Negative denormalized, Positive denormalized
 	int			cls			= _fpclass		(double(x));
 	if (cls&(_FPCLASS_SNAN+_FPCLASS_QNAN+_FPCLASS_NINF+_FPCLASS_PINF+_FPCLASS_ND+_FPCLASS_PD))	
 		return	false;	
 
 	/*	*****other cases are*****
 	_FPCLASS_NN Negative normalized non-zero 
-	_FPCLASS_NZ Negative zero ( – 0) 
+	_FPCLASS_NZ Negative zero ( â€“ 0) 
 	_FPCLASS_PZ Positive 0 (+0) 
 	_FPCLASS_PN Positive normalized non-zero 
 	*/
@@ -123,14 +123,14 @@ IC double	_sin	(double x)		{ return sin(x); }
 IC double	_cos	(double x)		{ return cos(x); }
 IC BOOL		_valid	(const double x)
 {
-	// check for: Signaling NaN, Quiet NaN, Negative infinity ( –INF), Positive infinity (+INF), Negative denormalized, Positive denormalized
+	// check for: Signaling NaN, Quiet NaN, Negative infinity ( â€“INF), Positive infinity (+INF), Negative denormalized, Positive denormalized
 	int			cls			= _fpclass		(x);
 	if (cls&(_FPCLASS_SNAN+_FPCLASS_QNAN+_FPCLASS_NINF+_FPCLASS_PINF+_FPCLASS_ND+_FPCLASS_PD))	
 		return false;	
 
 	/*	*****other cases are*****
 	_FPCLASS_NN Negative normalized non-zero 
-	_FPCLASS_NZ Negative zero ( – 0) 
+	_FPCLASS_NZ Negative zero ( â€“ 0) 
 	_FPCLASS_PZ Positive 0 (+0) 
 	_FPCLASS_PN Positive normalized non-zero 
 	*/
@@ -293,6 +293,21 @@ inline int __cdecl xr_sprintf	( char (&destination)[count], LPCSTR format_string
 	va_list args;
 	va_start					( args, format_string);
 	return						vsprintf_s( destination, count, format_string, args );
+}
+
+inline errno_t xr_strcpy(LPSTR destination, size_t const destination_size, LPCSTR source)
+{
+	return						strcpy_s(destination, destination_size, source);
+}
+
+template <int count>
+inline errno_t xr_strcpy	( char (&destination)[count], LPCSTR source )
+{
+	return						xr_strcpy( destination, count, source );
+}
+inline errno_t xr_strcat(LPSTR destination, size_t const buffer_size, LPCSTR source)
+{
+	return						strcat_s(destination, buffer_size, source);
 }
 XRCORE_API	char*				timestamp				(string64& dest);
 
